17361e800c1263dbcc78f8fb79239ac6a80bcb10
UjjwalKM22 <ujwalhoney@gmail.com>
2025-11-13 17:25:30 +0530

    Update coloredbox.js

--- coloredbox.js
+++ coloredbox.js
@@ -1,41 +1,47 @@
-(function() { 
-	let template = document.createElement("template");
-	template.innerHTML = `
-		<style>
-		:host {
-			border-radius: 25px;
-			border-width: 4px;
-			border-color: black;
-			border-style: solid;
-			display: block;
-		} 
-		</style> 
-	`;
+(function() {
+    let template = document.createElement("template");
+    template.innerHTML = `
+        <style>
+        :host {
+            border-radius: 25px;
+            border-width: 4px;
+            border-color: black;
+            border-style: solid;
+            display: block;
+        }
+        </style>
+    `;
 
-	class ColoredBox extends HTMLElement {
-		constructor() {
-			super(); 
-			let shadowRoot = this.attachShadow({mode: "open"});
-			shadowRoot.appendChild(template.content.cloneNode(true));
-			this.addEventListener("click", event => {
-				var event = new Event("onClick");
-				this.dispatchEvent(event);
-			});
-			this._props = {};
-		}
+    class ColoredBox extends HTMLElement {
+        constructor() {
+            super();
+            let shadowRoot = this.attachShadow({mode: "open"});
+            shadowRoot.appendChild(template.content.cloneNode(true));
+            this.addEventListener("click", () => {
+                this.dispatchEvent(new Event("onClick"));
+            });
+            this._props = {};
+        }
 
-		onCustomWidgetBeforeUpdate(changedProperties) {
-			this._props = { ...this._props, ...changedProperties };
-		}
+        onCustomWidgetBeforeUpdate(changedProperties) {
+            this._props = { ...this._props, ...changedProperties };
+        }
 
-		onCustomWidgetAfterUpdate(changedProperties) {
-			if ("color" in changedProperties) {
-				this.style["background-color"] = changedProperties["color"];
-			}
-			if ("opacity" in changedProperties) {
-				this.style["opacity"] = changedProperties["opacity"];
-			}
-		}
-	}
-      customElements.define("com-sap-sample-coloredbox", ColoredBox);
-  })();
+        onCustomWidgetAfterUpdate(changedProperties) {
+            if ("color" in changedProperties) {
+                this.style.backgroundColor = changedProperties["color"];
+            }
+            if ("opacity" in changedProperties) {
+                this.style.opacity = changedProperties["opacity"];
+            }
+            if ("width" in changedProperties) {
+                this.style.width = changedProperties["width"] + "px";
+            }
+            if ("height" in changedProperties) {
+                this.style.height = changedProperties["height"] + "px";
+            }
+        }
+    }
+
+    customElements.define("com-sap-sample-coloredbox", ColoredBox);
+})();
