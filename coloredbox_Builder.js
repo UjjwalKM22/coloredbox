f35eb27ef869acd2d998cb2eaa776cf65f20c68c
UjjwalKM22 <ujwalhoney@gmail.com>
2025-11-13 17:25:53 +0530

    Update coloredbox_Builder.js

--- coloredbox_Builder.js
+++ coloredbox_Builder.js
@@ -1,53 +1,47 @@
-(function()  {
-	let template = document.createElement("template");
-	template.innerHTML = `
-		<form id="form">
-			<fieldset>
-				<legend>Colored Box Properties</legend>
-				<table>
-					<tr>
-						<td>Opacity</td>
-						<td><input id="builder_opacity" type="text" size="5" maxlength="5"></td>
-					</tr>
-				</table>
-				<input type="submit" style="display:none;">
-			</fieldset>
-		</form>
-		<style>
-		:host {
-			display: block;
-			padding: 1em 1em 1em 1em;
-		}
-		</style>
-	`;
+(function() {
+    let template = document.createElement("template");
+    template.innerHTML = `
+        <form id="form">
+            <fieldset>
+                <legend>Colored Box Properties</legend>
+                <table>
+                    <tr>
+                        <td>Opacity</td>
+                        <td><input id="builder_opacity" type="number" step="0.1" min="0" max="1" size="5"></td>
+                    </tr>
+                </table>
+                <input type="submit" style="display:none;">
+            </fieldset>
+        </form>
+    `;
 
-	class ColoredBoxBuilderPanel extends HTMLElement {
-		constructor() {
-			super();
-			this._shadowRoot = this.attachShadow({mode: "open"});
-			this._shadowRoot.appendChild(template.content.cloneNode(true));
-			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
-		}
+    class ColoredBoxBuilderPanel extends HTMLElement {
+        constructor() {
+            super();
+            this._shadowRoot = this.attachShadow({mode: "open"});
+            this._shadowRoot.appendChild(template.content.cloneNode(true));
+            this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
+        }
 
-		_submit(e) {
-			e.preventDefault();
-			this.dispatchEvent(new CustomEvent("propertiesChanged", {
-					detail: {
-						properties: {
-							opacity: this.opacity
-						}
-					}
-			}));
-		}
+        _submit(e) {
+            e.preventDefault();
+            this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                detail: {
+                    properties: {
+                        opacity: this.opacity
+                    }
+                }
+            }));
+        }
 
-		set opacity(newOpacity) {
-			this._shadowRoot.getElementById("builder_opacity").value = newOpacity;
-		}
+        set opacity(newOpacity) {
+            this._shadowRoot.getElementById("builder_opacity").value = newOpacity;
+        }
 
-		get opacity() {
-			return this._shadowRoot.getElementById("builder_opacity").value;
-		}
-	}
+        get opacity() {
+            return this._shadowRoot.getElementById("builder_opacity").value;
+        }
+    }
 
-	customElements.define("com-sap-sample-coloredbox-builder", ColoredBoxBuilderPanel);
+    customElements.define("com-sap-sample-coloredbox-builder", ColoredBoxBuilderPanel);
 })();
